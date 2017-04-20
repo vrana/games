dd1d1727aa4dc998d4de1d4291db144f46dcbe6b
Jakub Vrana <jakub@vrana.cz>
2017-04-20 15:55:19 +0200

    Monopoly: Fit into 1430 width

--- monopoly/game.js
+++ monopoly/game.js
@@ -44,7 +44,7 @@ var fields = [
 function position(i) {
 	return {
 		top: 10 + 66 * (i < 10 ? 10 - i : i < 20 ? 0 : i < 30 ? i - 20 : 10),
-		left: 10 + 130 * (i < 10 ? 0 : i < 20 ? i - 10 : i < 30 ? 10 : 40 - i)
+		left: 10 + 128 * (i < 10 ? 0 : i < 20 ? i - 10 : i < 30 ? 10 : 40 - i)
 	};
 }
 
