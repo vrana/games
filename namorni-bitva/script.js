85c1807a5bf9835dc672ff10f647976883f6e8a0
Jakub Vrana <jakub@vrana.cz>
2019-08-01 11:56:29 +0200

    Namorni bitva: Sink boats

--- namorni-bitva/script.js
+++ namorni-bitva/script.js
@@ -22,7 +22,7 @@ function placeBoat(field, boat, x, y, place = false) {
 						return false; // The boat is outside the field.
 					}
 					for (let n=0; n < 9; n++) {
-						if (!isEmpty(field, x + i + n%3 - 1, y + j + Math.floor(n/3) - 1)) {
+						if (get(field, x + i + n%3 - 1, y + j + Math.floor(n/3) - 1)) {
 							return false; // There's something around this point.
 						}
 					}
@@ -59,16 +59,20 @@ let meField = createField();
 let youField = createField();
 
 for (let j=0; j < meField.length; j++) {
+	document.getElementById('me').appendChild(document.createElement('tr'));
+	document.getElementById('you').appendChild(document.createElement('tr'));
 	for (let i=0; i < meField[j].length; i++) {
+		document.getElementById('me').rows[j].appendChild(document.createElement('td'));
+		document.getElementById('you').rows[j].appendChild(document.createElement('td'));
 		if (meField[j][i]) {
 			setClassName('#me', i, j, 'boat');
 		}
 	}
 }
 
-document.querySelector('#you table').onclick = function (event) {
+document.querySelector('#you').onclick = function (event) {
 	var target = event.target;
-	if (target.tagName != 'TD') {
+	if (target.tagName != 'TD' || target.className) {
 		return;
 	}
 	let y = index(target.parentNode);
@@ -77,17 +81,18 @@ document.querySelector('#you table').onclick = function (event) {
 	do {
 		y = rand(meField.length);
 		x = rand(meField[y].length);
-	} while (meField[y][x] === '' || meField[y][x] === 'hit');
+	} while (meField[y][x] == '' || meField[y][x] == 'hit');
 	shoot(meField, x, y, '#me');
 };
 
 function shoot(field, x, y, id) {
 	if (field[y][x]) {
 		setFieldAndClassName(field, id, x, y, 'hit');
+		maybeSink(field, id, x, y, [x + "x" + y]);
 	} else {
 		setFieldAndClassName(field, id, x, y, 'water');
 		for (let i=1; i < field.length; i++) {
-			if (!isEmpty(field, x+i, y) || !isEmpty(field, x-i, y) || !isEmpty(field, x, y+i) || !isEmpty(field, x, y-i)) {
+			if (get(field, x+i, y) || get(field, x-i, y) || get(field, x, y+i) || get(field, x, y-i)) {
 				break;
 			}
 			setFieldAndClassName(field, id, x+i, y, 'water');
@@ -96,6 +101,25 @@ function shoot(field, x, y, id) {
 			setFieldAndClassName(field, id, x, y-i, 'water');
 		}
 	}
+}
+
+function maybeSink(field, id, x, y, checked) {
+	for (let [i, j] of [[1,0], [-1,0], [0,1], [0,-1]]) {
+		let s = (x+i) + "x" + (y+j);
+		if (checked.indexOf(s) == -1 && (
+				get(field, x+i, y+j) == 1
+				|| (get(field, x+i, y+j) == 'hit' && !maybeSink(field, id, x+i, y+j, checked.concat(s))))) {
+			return false;
+		}
+	}
+	for (let j=-1; j <= 1; j++) {
+		for (let i=-1; i <= 1; i++) {
+			if (get(field, x+i, y+j) == undefined) {
+				setFieldAndClassName(field, id, x+i, y+j, 'water');
+			}
+		}
+	}
+	return true;
 }
 
 function setFieldAndClassName(field, id, x, y, className) {
@@ -115,9 +139,9 @@ function setClassName(id, x, y, className) {
 	}
 }
 
-function isEmpty(field, x, y) {
+function get(field, x, y) {
 	let row = field[y];
-	return !row || !row[x];
+	return row && row[x];
 }
 
 function index(element) {
