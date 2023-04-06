9887c2cb18c907f28536e47a9b34a852ce95b161
Jakub Vrána <jakub@vrana.cz>
2023-04-06 08:57:17 +0200

    Dostihy: Remove Jana

--- monopoly/game.js
+++ monopoly/game.js
@@ -222,7 +222,7 @@ var playing = -1;
 	var board = document.body.appendChild(createDom('div', {id: 'board'}));
 
 	var scoreTable = board.appendChild(createDom('table', {id: 'score'}));
-	var defaultNames = ['Jakub', 'Kryštof', 'Prokop', 'Jana'];
+	var defaultNames = ['Jakub', 'Kryštof', 'Prokop'];
 	for (var i = 0; i < maxPlayers; i++) {
 		scoreTable.appendChild(createScoreRow(i, defaultNames[i]));
 	}
